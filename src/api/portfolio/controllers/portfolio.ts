62ff79c72a706efb91769cf0d77bcb8425408460
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-12-09 09:24:56 +0200

    changed: portfolio controller

--- src/api/portfolio/controllers/portfolio.ts
+++ src/api/portfolio/controllers/portfolio.ts
@@ -7,16 +7,7 @@ import { factories } from "@strapi/strapi";
 const uid = "api::portfolio.portfolio";
 
 export default factories.createCoreController(uid, ({ strapi }) => ({
-  async find() {
-    return await strapi.documents(uid).findMany({
-      populate: {
-        contacts: { populate: { icon: { fields: ["url"] } } },
-        expertise: {
-          populate: { icon: { fields: ["url"] }, stackList: true },
-        },
-        careerHistory: true,
-        projects: { populate: { banner: { fields: ["url"] } } },
-      },
-    });
+  async find(ctx) {
+    return await strapi.documents(uid).findMany(ctx.query);
   },
 }));
