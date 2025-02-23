0e31779f80a781bc0544bd8486f7a13daa335943
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2025-02-23 18:15:39 +0200

    misc changes
    added: category filter functional
    changed: strapi version

--- src/api/category/controllers/category.ts
+++ src/api/category/controllers/category.ts
@@ -2,6 +2,20 @@
  * category controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::category.category');
+export default factories.createCoreController(
+  "api::category.category",
+  (ctx) => ({
+    async find(ctx) {
+      ctx.query = { populate: { projects: { fields: ["id"] } }, ...ctx.query };
+      const { data, meta } = await super.find(ctx);
+      const filteredData = data.filter((el) => !!el.projects.length);
+      meta.pagination = { ...meta.pagination, total: filteredData.length };
+      return {
+        data: filteredData,
+        meta,
+      };
+    },
+  })
+);
